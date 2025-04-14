a07e3b3c6c6584056ddade08778c6b5de02d1911
Raffaele Montella <raffaele.montella@uniparthenope.it>
2025-04-14 14:29:12 +0200

    New file

---
+++ MyCalc/app/src/main/java/it/raffaelemontella/mycalc/MainActivity.kt
@@ -0,0 +1,21 @@
+package it.raffaelemontella.mycalc
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button9).setOnClickListener {
+            val display = findViewById<TextView>(R.id.textDisplay)
+            display.text = "9"
+        }
+    }
+}
